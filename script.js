dd2214a0b5e10c78e99926354bb08122cdf55670
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-07-19 22:00:11 -0300

    Adicionando o async e o await no javascript.

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-function Ceps() {
-    const cep = document.getElementById('cep').value
-    const lugar = document.getElementById('resul')
+async function Ceps() {
+    const cep = document.getElementById('cep').value;
+    const lugar = document.getElementById('resul');
 
-    fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(res => res.json())
-    .then(data => {
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
 
-    if (data.erro) {
-        lugar.innerHTML = "CEP não encontrado.";
+        if (data.erro) {
+            lugar.innerHTML = "CEP não encontrado.";
+        } else {
+            lugar.innerHTML = `${data.logradouro}, ${data.bairro} <br/>Estado: ${data.estado}, ${data.uf} </br> Região: ${data.regiao}`;
+        }
+    } catch (error) {
+        lugar.innerHTML = "Erro ao buscar o CEP. Tente novamente.";
+        console.error("Falha na requisição:", error);
     }
-
-    else {
-        lugar.innerHTML =  `${data.logradouro}, ${data.bairro} <br/>Estado: ${data.estado}, ${data.uf} </br> Região: ${data.regiao}`
-    }
-
-    })
-
-    .catch(error => {
-            lugar.innerHTML = "Erro ao buscar o CEP. Tente novamente.";
-            console.error("Falha na requisição:", error);
-        })
-
 }
