599e9f5a234e8d7be17e054253083f99ee090ff2
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2026-04-29 00:02:05 -0300

    Formatando os arquivos style.css, script.js e README.md.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 async function Ceps() {
-    const cep = document.getElementById('cep').value;
-    const lugar = document.getElementById('resul');
+  const cep = document.getElementById("cep").value;
+  const lugar = document.getElementById("resul");
 
-    try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const data = await response.json();
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
 
-        if (data.erro) {
-            lugar.innerHTML = "CEP não encontrado.";
-        } else {
-            lugar.innerHTML = `${data.logradouro}, ${data.bairro} <br/>Estado: ${data.estado}, ${data.uf} </br> Região: ${data.regiao}`;
-        }
-    } catch (error) {
-        lugar.innerHTML = "Erro ao buscar o CEP. Tente novamente.";
-        console.error("Falha na requisição:", error);
+    if (data.erro) {
+      lugar.innerHTML = "CEP não encontrado.";
+    } else {
+      lugar.innerHTML = `${data.logradouro}, ${data.bairro} <br/>Estado: ${data.estado}, ${data.uf} </br> Região: ${data.regiao}`;
     }
+  } catch (error) {
+    lugar.innerHTML = "Erro ao buscar o CEP. Tente novamente.";
+    console.error("Falha na requisição:", error);
+  }
 }
